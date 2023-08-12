a0aef684617a9f87530afb511b980e629c36ff0b
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2023-08-12 19:20:08 +0530

    Home-page layout

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+// export const apiKey = 'place your openAI api key here';
+// in some cases your api key maybe already expired
+// try to use a new account to create an api key
+
+export const dummyMessages = [
+    {
+        id: 1,
+        role: 'user', 
+        content: 'How are you?'
+    },
+    {
+        id: 2,
+        role: 'assistant',
+        content: "I'm fine, How may i help you today."
+    },
+    {
+        id: 3,
+        role: 'user',
+        content: 'create an image of a dog playing with cat'
+    },
+    {
+        id: 4,
+        role: 'assistant',
+        content: 'https://storage.googleapis.com/pai-images/ae74b3002bfe4b538493ca7aedb6a300.jpeg'
+    }
+]
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,202 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+// import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import Voice from '@react-native-community/voice';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+// import { apiCall } from '../api/openAI';
+import Features from '../components/features';
+import { dummyMessages } from '../constants';
+// import Tts from 'react-native-tts';
 
 export default function HomeScreen() {
+
+  const [messages, setMessages] = useState(dummyMessages);
+  const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] = useState(false);
+
+
+  // //! Start-Stop Recording
+  const startRecording = async () => {
+    setRecording(true);
+    // Tts.stop(); 
+    // try {
+    //   await Voice.start('en-GB'); // en-US
+
+    // } catch (error) {
+    //   console.log('error', error);
+    // }
+  };
+  const stopRecording = async () => {
+    setRecording(false);
+  };
+
+
+
+  // //! Button Functions
+
+  const clear = () => {
+    // Tts.stop();
+    // setSpeaking(false);
+    // setLoading(false);
+    setMessages([]);
+  };
+
+  const stopSpeaking = () => {
+    // Tts.stop();
+    setSpeaking(false);
+  }
+
+
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+      {/* <StatusBar barStyle="dark-content" /> */}
+      <SafeAreaView className="flex-1 flex mx-5">
+        {/* bot icon */}
+        <View className="flex-row justify-center">
+          <Image
+            source={require('../../assets/images/bot.png')}
+            style={{ height: hp(15), width: hp(15) }}
+          />
+        </View>
+
+
+        {/* //!  features || message history  */}
+        {
+          messages.length > 0 ? (
+            <View className="space-y-2 flex-1">
+              <Text className="text-gray-700 font-semibold ml-1" style={{ fontSize: wp(5) }}>Assistant</Text>
+
+              <View
+                style={{ height: hp(58) }}
+                className="bg-neutral-200 rounded-3xl p-4">
+                <ScrollView
+                  // ref={scrollViewRef} 
+                  bounces={false}
+                  className="space-y-4"
+                  showsVerticalScrollIndicator={false}
+                >
+                  {
+                    messages.map((message, index) => {
+                      if (message.role == 'assistant') {
+                        if (message.content.includes('https')) {
+                          // result is an ai image
+                          return (
+                            <View key={index} className="flex-row justify-start">
+                              <View
+                                className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                                <Image
+                                  source={{ uri: message.content }}
+                                  className="rounded-2xl"
+                                  resizeMode="contain"
+                                  style={{ height: wp(60), width: wp(60) }}
+                                />
+                              </View>
+                            </View>
+
+
+                          )
+                        } else {
+                          // chat gpt response
+                          return (
+                            <View
+                              key={index} style={{ width: wp(70) }}
+                              className="bg-emerald-100 p-2 rounded-xl rounded-tl-none">
+                              <Text className="text-neutral-800" style={{ fontSize: wp(4) }}  >
+                                {message.content}
+                              </Text>
+                            </View>
+                          )
+                        }
+                      } else {
+                        // user input text
+                        return (
+                          <View key={index} className="flex-row justify-end">
+                            <View
+                              style={{ width: wp(70) }}
+                              className="bg-white p-2 rounded-xl rounded-tr-none">
+                              <Text style={{ fontSize: wp(4) }}  >
+                                {message.content}
+                              </Text>
+                            </View>
+                          </View>
+                        );
+                      }
+
+
+                    })
+                  }
+                </ScrollView>
+              </View>
+            </View>
+          ) : (
+            <Features />
+          )
+        }
+
+
+        {/* //! recording, clear and stop buttons */}
+        <View className="flex justify-center items-center">
+          {
+
+            recording ? (
+              <TouchableOpacity className="space-y-2" onPress={stopRecording}>
+                {/* recording stop button */}
+                <Image
+                  className="rounded-full"
+                  source={require('../../assets/images/voiceLoading.gif')}
+                  style={{ width: hp(10), height: hp(10) }}
+                />
+              </TouchableOpacity>
+
+            ) : (
+              <TouchableOpacity onPress={startRecording}>
+                {/* recording start button */}
+                <Image
+                  className="rounded-full"
+                  source={require('../../assets/images/recordingIcon.png')}
+                  style={{ width: hp(10), height: hp(10) }}
+                />
+              </TouchableOpacity>
+            )
+          }
+          {
+            messages.length > 0 && (
+              <TouchableOpacity
+                onPress={clear}
+                className="bg-neutral-400 rounded-3xl p-2 absolute right-10"
+              >
+                <Text className="text-white font-semibold">Clear</Text>
+              </TouchableOpacity>
+            )
+          }
+          {
+            speaking && (
+              <TouchableOpacity
+                onPress={stopSpeaking}
+                className="bg-red-400 rounded-3xl p-2 absolute left-10"
+              >
+                <Text className="text-white font-semibold">Stop</Text>
+              </TouchableOpacity>
+            )
+          }
+
+
+
+        </View>
+
+      </SafeAreaView>
+
+
+
     </View>
   )
 }
